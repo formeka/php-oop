0044ea7c30f9ade0f0bc5bf75876cf510f769d6b
Mehdi Key <formk@kermed.fr>
2023-12-04 14:37:45 +0100

    Passage de parametre à une methode

--- MaClass.php
+++ MaClass.php
@@ -2,14 +2,46 @@
 
 class MaClass
 {
-    public $attributPublic;
-    public $couleur = 'vert';
-    public $age = 54;
-    private $_attributPrivate;
-    private $_nom = 'Michel PLIK';
+    public string $attributPublic;
+    public string $couleur = 'vert';
+    public int $age = 54;
+    public string $prenom;
+    public const PI = 3.14;
+    public float|int $rayon;
+
+    // private string $_attributPrivate;
+    // private string $_nom = 'Michel PLIK';
+
+
+    public function displayMethode($value) : string
+    {
+        //echo 'Je suis une methode .';
+        // echo 'Je suis une methode qui affiche ' . $value;
+        return $this->prenom . ' aime la couleur ' . $this->couleur . ' ' . $value;
+    }
+
+public function calculCercleAire(): float|int
+{
+    // echo self::PI;
+    return self::PI * $this->rayon * $this->rayon;
 }
 
-$obj = new MaClass();
-echo 'Couleur : ' . $obj->couleur . '<br>';
-echo 'Age: ' . $obj->age . '<br>';
+public function calculCercleAire2(float|int $rayonParam) : float|int
+{
+    return self::PI * $rayonParam * $rayonParam;
+}
+
+}
+
+$objClass = new MaClass();
+//echo 'Couleur : ' . $obj->couleur . '<br>';
+//echo 'Age: ' . $obj->age . '<br>';
 //echo 'Nom: ' . $obj->_nom;
+
+// $objClass->prenom = 'Bill';
+// echo $objClass->displayMethode('Coucou');
+
+$objClass->rayon = 3.5;
+echo $objClass->calculCercleAire() . 'cm2<br>';
+
+echo $objClass->calculCercleAire2(3.5) . 'cm2';
